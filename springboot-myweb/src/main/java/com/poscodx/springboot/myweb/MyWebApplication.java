a11cce45bec9dc2d08d61bd9945153ecb217f57c
sso <sojeong7524@gmail.com>
2023-10-10 15:53:19 +0900

    myweb #finish

---
+++ springboot-myweb/src/main/java/com/poscodx/springboot/myweb/MyWebApplication.java
@@ -0,0 +1,29 @@
+package com.poscodx.springboot.myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class MyWebApplication {
+
+	@Controller
+	public class HellController {
+		@ResponseBody
+		@RequestMapping("/hello")
+		public String hello() {
+			return "Hello World!!!!!";
+		}
+
+		@RequestMapping("/hello2")
+		public String hello2() {
+			return "hello";
+		}		
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebApplication.class, args);
+	}
+}
